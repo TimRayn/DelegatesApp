a10efe017a2e67457d4bab497c3e199f83ba4081
Tim Rayn <maxfoxlex@gmail.com>
2019-03-14 12:02:51 +0200

    Add project files.

---
+++ DelegatesApp/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesApp
+{
+    class Account
+    {
+        // Объявляем делегат
+        public delegate void AccountStateHandler(object sender, AccountEventArgs e);
+        // Событие, возникающее при выводе денег
+        public event AccountStateHandler Withdrawn;
+        // Событие, возникающее при добавлении на счет
+        public event AccountStateHandler Added;
+
+        int _sum; // Переменная для хранения суммы
+
+        public Account(int sum)
+        {
+            _sum = sum;
+        }
+
+        public int CurrentSum
+        {
+            get { return _sum; }
+        }
+
+        public void Put(int sum)
+        {
+            _sum += sum;
+            if (Added != null)
+                Added(this, new AccountEventArgs($"На счет поступило {sum}", sum));
+        }
+        public void Withdraw(int sum)
+        {
+            if (_sum >= sum)
+            {
+                _sum -= sum;
+                if (Withdrawn != null)
+                    Withdrawn(this, new AccountEventArgs($"Сумма {sum} снята со счета", sum));
+            }
+            else
+            {
+                if (Withdrawn != null)
+                    Withdrawn(this, new AccountEventArgs("Недостаточно денег на счете", sum));
+            }
+        }
+    }
+}
---
+++ DelegatesApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(200);
+            // Добавляем обработчики события
+            account.Added += Show_Message;
+            account.Withdrawn += Show_Message;
+
+            account.Withdraw(100);
+            // Удаляем обработчик события
+            account.Withdrawn -= Show_Message;
+
+            account.Withdraw(50);
+            account.Put(150);
+
+            Console.ReadLine();
+        }
+        private static void Show_Message(object sender, AccountEventArgs e)
+        {
+            Console.WriteLine($"Сумма транзакции: {e.Sum}");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
